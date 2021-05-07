180122094961d1bf3aa4ed0c8626fed50fac31df
mgitto1 <matthew.gitto1@gmail.com>
2021-05-07 16:42:55 -0400

    Feat: FE connection to docker complete; BE connection in work

--- server/docker/codeTest.js
+++ server/docker/codeTest.js
@@ -1,11 +1,1 @@
-console.log("Hello World");
-
-console.log("Hello Again");
-
-const sum = (a, b) => {
-  // const num = a;
-  // num = b;
-  return a + b;
-};
-
-console.log(sum(1, 2));
+console.log('hello')
