1b19d71fa035ac1d7785ab1634b37ffac4305635
Altus Szawlowski <altusszawlowski@gmail.com>
2021-05-11 14:36:09 -0400

    FE: fixes: changed input for submit form to textare

--- client/components/SingleIssue.js
+++ client/components/SingleIssue.js
@@ -20,7 +20,7 @@ class SingleIssue extends React.Component {
         <h2>EXPLANATION SECTION</h2>
         <form>
           <label>
-            <input type="text" name="name" />
+            <textarea type="text" name="name" />
           </label>
           <input type="submit" value="Submit" />
         </form>
