48bbf77528c45dfc40b87853172662ecdec105fd
Nathan Damico <nathandamico5@gmail.com>
2021-05-11 15:05:55 -0400

    FE - Set up state for PostIssue

--- client/components/PostIssue.js
+++ client/components/PostIssue.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+// import axios from 'axios';
 
 const PostIssue = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [tags, setTags] = useState("");
+
+  const handleSubmit = async () => {
+    // await axios.post()
+  };
+
+  console.log(title);
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Title:
           <input
             type="text"
             name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             placeholder="e.g. Please help me solve this algorithm"
           />
         </label>
         <label>
-          Issue:
-          <textarea type="text" name="issue" />
+          Description:
+          <textarea
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </label>
         <label>
           Tags:
-          <input type="text" name="tags" placeholder="Javascript" />
+          <input
+            type="text"
+            name="tags"
+            placeholder="Javascript"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
         </label>
       </form>
     </div>
--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { withRouter, Route, Switch } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
 import AllIssues from "./components/AllIssues";
@@ -29,7 +29,7 @@ class Routes extends Component {
           </Switch>
         ) : (
           <Switch>
-            <Route exact path="/" exact component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/issues" component={AllIssues} />
