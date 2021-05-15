142f09a7fbf1f54000511fff1188e7d330e174ef
Nathan Damico <nathandamico5@gmail.com>
2021-05-14 20:43:56 -0400

    BE: update Pass error to next in route

--- server/api/stats.js
+++ server/api/stats.js
@@ -1,11 +1,13 @@
-const router = require('express').Router();
-const { User, Issue, Solution, Stat } = require('../db');
-const { requireToken } = require('./authMiddleware');
+const router = require("express").Router();
+const { User, Issue, Solution, Stat } = require("../db");
+const { requireToken } = require("./authMiddleware");
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const stats = await Stat.findAll();
     res.json(stats);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 });
