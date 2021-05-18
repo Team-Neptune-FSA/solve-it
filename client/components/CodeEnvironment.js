1e7d5a61d45c8d73e569b80d6e807a8a03b12bf6
Altus Szawlowski <altusszawlowski@gmail.com>
2021-05-18 12:49:09 -0400

    FE: Style: created a black box for code output

--- client/components/CodeEnvironment.js
+++ client/components/CodeEnvironment.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Editor from '@monaco-editor/react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Editor from "@monaco-editor/react";
 
-function codeEnvironment({setSolutionCode, value}) {
-  const [code, setCode] = useState('//enter code here...');
-  const [output, setOutput] = useState('');
-  
+function codeEnvironment({ setSolutionCode, value }) {
+  const [code, setCode] = useState("//enter code here...");
+  const [output, setOutput] = useState("");
+
   useEffect(() => {
     console.log(value); //returns undefined
     if (value) {
-      setCode(value)
+      setCode(value);
     }
-  }, [value]);  //this renders the value from the DB if present???
+  }, [value]); //this renders the value from the DB if present???
 
   const handleSubmit = async () => {
     console.log(code);
-    const { data: output } = await axios.post('/api/execute', { code });
+    const { data: output } = await axios.post("/api/execute", { code });
     setOutput(output);
   };
 
@@ -25,23 +25,23 @@ function codeEnvironment({setSolutionCode, value}) {
   };
 
   return (
-    <div>
-      <Editor //main editor
-        height="50vh"
-        width="75vw"
-        value={code}
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        onChange={handleChange}
-        options={{ readOnly: false }}
-      />
-      <br />
-      
-      <div>{output}</div> {/* this is where the output lives */}
-
+    <>
+      <div className="code-environment-container">
+        <Editor //main editor
+          height="50vh"
+          width="75vw"
+          value={code}
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          onChange={handleChange}
+          options={{ readOnly: false }}
+        />
+        <br />
+        <div className="output-box">{output}</div>
+        {/* this is where the output lives */}
+      </div>
       <button onClick={() => handleSubmit()}>Run Code</button>
-
-    </div>
+    </>
   );
 }
 
