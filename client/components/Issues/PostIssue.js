54f532357e84b290cce063103e4c63ec2a6e0768
Nathan Damico <nathandamico5@gmail.com>
2021-05-20 17:45:55 -0400

    FE/BE: feat Began implementation of stripe

--- client/components/Issues/PostIssue.js
+++ client/components/Issues/PostIssue.js
@@ -6,11 +6,19 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
+// Stripe
+// import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
 const PostIssue = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [language, setLanguage] = useState("javascript");
+
+  // Stripe
+  // const stripe = useStripe();
+  // const elements = useElements();
+
   const notifySubmit = () =>
     toast("Issue Posted!", { position: toast.POSITION.BOTTOM_RIGHT });
 
@@ -60,8 +68,6 @@ const PostIssue = () => {
   const handleSubmit = async () => {
     const token = window.localStorage.getItem("token");
     const issuePrice = parseFloat(price) * 100;
-    console.log(issuePrice);
-    console.log(typeof issuePrice);
     await axios.post(
       "/api/issues",
       {
@@ -82,6 +88,34 @@ const PostIssue = () => {
       setPrice(e.target.value);
     }
   };
+
+  // const handleStripeSubmit = async (event) => {
+  //   // Block native form submission.
+  //   event.preventDefault();
+
+  //   if (!stripe || !elements) {
+  //     // Stripe.js has not loaded yet. Make sure to disable
+  //     // form submission until Stripe.js has loaded.
+  //     return;
+  //   }
+
+  //   // Get a reference to a mounted CardElement. Elements knows how
+  //   // to find your CardElement because there can only ever be one of
+  //   // each type of element.
+  //   const cardElement = elements.getElement(CardElement);
+
+  //   // Use your card Element with other Stripe.js APIs
+  //   const { error, paymentMethod } = await stripe.createPaymentMethod({
+  //     type: "card",
+  //     card: cardElement,
+  //   });
+
+  //   if (error) {
+  //     console.log("[error]", error);
+  //   } else {
+  //     console.log("[PaymentMethod]", paymentMethod);
+  //   }
+  // };
 
   return (
     <>
@@ -114,6 +148,7 @@ const PostIssue = () => {
               >
                 <option value="javascript">Javascript</option>
               </select>
+
               <div className="flex input-field">
                 <label>Price: $ </label>
                 <input
@@ -161,6 +196,27 @@ const PostIssue = () => {
                 Submit Request
               </button>
             </form>
+            {/* <form onSubmit={handleStripeSubmit}>
+              <CardElement
+                options={{
+                  style: {
+                    base: {
+                      fontSize: "16px",
+                      color: "#424770",
+                      "::placeholder": {
+                        color: "#aab7c4",
+                      },
+                    },
+                    invalid: {
+                      color: "#9e2146",
+                    },
+                  },
+                }}
+              />
+              <button type="submit" disabled={!stripe}>
+                Pay
+              </button>
+            </form> */}
           </div>
         </div>
       ) : (
